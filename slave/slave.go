a41e89d4cdd9fa8810eea3c92acdfe1d03daec25
Ryan Plante <rplante97@gmail.cim>
2019-04-19 15:42:28 -0400

    Slave

--- slave/slave.go
+++ slave/slave.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strconv"
+	"time"
 )
 
 func main() {
+	//Global for now
+	m := make(map[string]string)
 
 	if len(os.Args) != 2 {
 		fmt.Fprintf(os.Stderr, "Usage: %s HOST:PORT\n", os.Args[0])
@@ -19,7 +24,86 @@ func main() {
 		fmt.Fprintln(os.Stderr, err)
 		return
 	}
+	reader := bufio.NewReader(conn)
+	//Grab first line (GET or SET)
+	for true {
+		line, isPrefix, err := reader.ReadLine()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return
+		}
+		if isPrefix == true {
+			fmt.Println("Error: Line too long!")
+		}
+		var op = string(line)
+		fmt.Printf("Operation: %s\n", op)
 
-	_ = conn
+		//Grab key value
+		line, isPrefix, err = reader.ReadLine()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return
+		}
+		if isPrefix == true {
+			fmt.Println("Error: Line too long!")
+		}
 
+		var key = string(line)
+		fmt.Printf("Key %s\n", key)
+
+		//GET and SET routines
+		if op == "get" {
+			//TODO get(key)
+			ret := m[key]
+			var strlen = len(ret)
+			fmt.Fprintf(conn, "%d\n", strlen)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				return
+			}
+			_, err = conn.Write([]byte(ret))
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				return
+			}
+			fmt.Println("GET hit")
+			fmt.Printf("Length %d, Data: %s\n", strlen, ret)
+
+		} else if op == "set" {
+			//Read data length
+			line, isPrefix, err = reader.ReadLine()
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				return
+			}
+			if isPrefix == true {
+				fmt.Println("Error: Line too long!")
+			}
+			var datalen = string(line)
+			fmt.Printf("Data length: %s\n", datalen)
+
+			//Read in data
+			datalenint, _ := strconv.Atoi(datalen)
+			data := make([]byte, datalenint)
+			_, _ = reader.Read(data)
+
+			s := string(data)
+			fmt.Printf("Data: %s\n", s)
+			m[key] = s
+
+		} else {
+			fmt.Println("Error: Unexpected operation. Expect \"get\" or \"set\"")
+			return
+		}
+		time.Sleep(1 * time.Second)
+	}
 }
+
+//func get() {
+
+//}
+
+//func set(key, data) err {
+
+//	return
+//}
