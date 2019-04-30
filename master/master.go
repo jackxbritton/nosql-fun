44d022b60f420e9a5b4b49c555fa70d960dd03f9
Jack Britton <jack.britton@whiteops.com>
2019-04-30 09:47:19 -0400

    Whoops hex dump.

--- master/master.go
+++ master/master.go
@@ -3,8 +3,8 @@ package main
 import (
 	"bufio"
 	"crypto/md5"
-	"encoding"
 	"encoding/binary"
+	"encoding/hex"
 	"fmt"
 	"io"
 	"log"
@@ -103,7 +103,7 @@ func main() {
 		slaveIndex := binary.LittleEndian.Uint32(hash) % uint32(len(slaves))
 
 		fmt.Printf("getting key '%s'\n", key)
-		fmt.Printf("md5(%s) = %s\n", key, encoding.Dump(hash))
+		fmt.Printf("md5(%s) = %s\n", key, hex.Dump(hash))
 		fmt.Printf("slave index = %d\n", slaveIndex)
 
 		// Write request to the lucky slave.
